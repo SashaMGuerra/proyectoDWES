1483e6494d2a99a83cf0eba1d0b6e0abf567cadf
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-10-18 08:56:23 +0200

    18/10/2021
    Inicio repositorio.

---
+++ indexProyectoDWES.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>DAW2</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="webroot/css/common.css" rel="stylesheet" type="text/css"/>
+        <link href="webroot/css/indexDWES.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <h2>Proyecto DWES</h2>
+        </header>
+        <main>
+            <ul>
+                <li>Tema 1: Desarrollo web en entorno servidor</li>
+                <li>Tema 2: Instalación, configuración y documentación del entorno de desarrollo y del entorno de explotación</li>
+                <li><a href="../proyectoTema3/indexProyectoTema3.php">Tema 3: Características del lenguaje PHP</a></li>
+                <li>Tema 4: Técnicas de acceso a datos en PHP</li>
+                <li>Tema 5: Desarrollo de aplicaciones web utilizando código embebido</li>
+                <li>Tema 6: Programación orientada a objetos en PHP</li>
+                <li>Tema 7: Programación de servicios web</li>
+                <li>Tema 8: Desarrollo de aplicaciones web híbridas</li>
+                <li>Tema 9: Desarrollo de aplicaciones web híbridas</li>
+            </ul>
+        </main>
+        <footer>
+            <div>Modificado el 06/10/2021 - Mª Isabel Martínez Guerra</div>
+            <div><a href="https://github.com/SashaMGuerra/proyectoWeikis"><img id="github" src="webroot/media/img/github_logo_white.png" alt="github logo"/></a></div>
+        </footer>
+    </body>
+</html>
+
