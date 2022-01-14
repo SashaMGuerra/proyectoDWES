8efab4aa48898effbb1e37afd07e04cd4d6cbdbd
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2022-01-14 09:04:24 +0100

    14/01/2022 Estilo
    Cambio en el estilo: aplicado grid.

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
         <title>DAW2</title>
         <meta charset="UTF-8">
@@ -7,29 +8,25 @@
     </head>
     <body>
         <header>
-            <a class="volver" href="../index.php" target="_blank">Volver</a>
+            <a class="volver" href="../index.html">Volver</a>
             <h1>Desarrollo Web en Entorno Servidor</h1>
             <h2>Proyecto DWES</h2>
         </header>
         <main>
-            <ul>
-                <li><a target="_blank" href="doc/EjerciciosTema1.pdf">Tema 1: Desarrollo web en entorno servidor</a></li>
-                <li><a target="_blank" href="doc/DocumentaciónTema2US.pdf">Tema 2: Instalación, configuración y documentación del entorno de desarrollo y del entorno de explotación</a></li>
-                <li><a target="_blank" href="../proyectoTema3/indexProyectoTema3.php">Tema 3: Características del lenguaje PHP</a></li>
-                <li><a target="_blank" href="../proyectoTema4/indexProyectoTema4.php">Tema 4: Técnicas de acceso a datos en PHP</a></li>
-                <li class="proyecto"><a target="_blank" href="../MtoDepartamentosTema4/index.php">Mantenimiento Departamentos Tema 4</a></li>
-                <li><a target="_blank" href="../proyectoTema5/indexProyectoTema5.php">Tema 5: Desarrollo de aplicaciones web utilizando código embebido</a></li>
-                <li class="proyecto"><a target="_blank" href="../LoginLogoffTema5/indexLoginLogoffTema5.php">Proyecto Login-Logoff</a></li>
-                <li class="proyecto"><a target="_blank" href="../AplicacionLoginLogout/">Aplicación Login-Logout</a></li>
-                <li>Tema 6: Programación orientada a objetos en PHP</li>
-                <li>Tema 7: Programación de servicios web</li>
-                <li>Tema 8: Desarrollo de aplicaciones web híbridas</li>
-                <li>Tema 9: Desarrollo de aplicaciones web híbridas</li>
-            </ul>
+            <div class="container">
+                <a target="_blank" href="doc/EjerciciosTema1.pdf">Tema 1: Desarrollo web en entorno servidor</a>
+                <a target="_blank" href="doc/DocumentaciónTema2US.pdf">Tema 2: Instalación, configuración y documentación del entorno de desarrollo y del entorno de explotación</a>
+                <a target="_blank" href="../proyectoTema3/indexProyectoTema3.php">Tema 3: Características del lenguaje PHP</a>
+                <a target="_blank" href="../proyectoTema4/indexProyectoTema4.php">Tema 4: Técnicas de acceso a datos en PHP</a>
+                <a class="proyecto" target="_blank" href="../MtoDepartamentosTema4/index.php">Mantenimiento Departamentos Tema 4</a>
+                <a target="_blank" href="../proyectoTema5/indexProyectoTema5.php">Tema 5: Desarrollo de aplicaciones web utilizando código embebido</a>
+                <a class="proyecto" target="_blank" href="../LoginLogoffTema5/indexLoginLogoffTema5.php">Aplicación Login-Logout Tema 5</a>
+                <a class="proyecto" target="_blank" href="../AplicacionLoginLogout/">Aplicación Login-Logout</a>
+            </div>    
         </main>
         <footer>
             <a href="https://github.com/SashaMGuerra/proyectoDWES" target="_blank"><img src="../webroot/media/img/github_logo_white.png" alt="repositorio"></a>
-            <div>© 2021-2022 Isabel Martínez Guerra — IES Los Sauces (Benavente, Zamora) — Modificado el 26/11/2021.</div>
+            <div>© 2021-2022 Isabel Martínez Guerra — IES Los Sauces (Benavente, Zamora) — Modificado el 14/01/2022.</div>
         </footer>
 
     </body>
